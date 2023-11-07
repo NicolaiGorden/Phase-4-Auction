fb50fb67b2601a6efe600f60e2cad0a14a12d250
Nicolai <Nicolaigorden@gmail.com>
2023-11-07 12:59:34 -0500

    user validations and creation up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validates :username, presence: true, uniqueness: {message: "already belongs to another user!"}
+    validate :password, :must_contain_uppercase
+
+    def must_contain_uppercase
+        if password != nil
+            unless password.match(/[[:upper:]]/)
+                errors.add(:password, message: "must contain uppercase character!")
+            end
+        end
+    end
 end
