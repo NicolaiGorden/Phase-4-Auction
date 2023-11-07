a55be1d720a3955abf8ab3a7060ae7df2fe9ceb5
Nicolai <Nicolaigorden@gmail.com>
2023-11-07 13:48:15 -0500

    setup auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,13 @@ class UsersController < ApplicationController
     end
 
     def show
-        user = User.find(params[:id])
-        render json: user
-    end
+        user = User.find_by(id: session[:user_id])
+        if user
+          render json: user
+        else
+          render json: { error: "Not authorized" }, status: :unauthorized
+        end
+      end
 
     def create
         user = User.create(user_params)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get '/hello', to: 'application#hello_world'
-  # post "/login" to: "sessions#create"
 
+  #SESSION
+  post "/login", to: "sessions#create"
+  get "/me", to: "users#show"
+  delete "/logout", to: "sessions#destroy"
+
+  #USERS
   resources :users, only: [:index, :create, :show]
 
   get '*path',
